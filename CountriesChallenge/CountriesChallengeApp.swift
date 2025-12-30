18d35b027ca74c4dc9d94f1e3a4a0f029a883661
Matthew Ogtong <ogtongm@gmail.com>
2025-12-30 18:14:44 -0500

    Initial Commit

---
+++ CountriesChallenge/CountriesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesChallengeApp.swift
+//  CountriesChallenge
+//
+//  Created by Matthew Ogtong on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
